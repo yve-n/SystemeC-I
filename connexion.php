5266713e295fbf632a787ff35b74067923d9f431
yve-n <yve.nguetche@gmail.com>
2021-10-15 03:25:36 +0200

    create session and form connect validate

---
+++ connexion.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+require_once 'config.php';
+
+if(isset($_POST['email']) && isset($_Post['password'])){
+
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+
+    // verifier si la personne est bien inscrite dans la base
+    $check = $bdd->prepare('SELECT pseudo, email, password FROM utilisateurs WHERE email = ?');
+    $check->execute(array($email));
+    $data = $check->fetch();
+    $row = $check->rowCount();
+
+    if($row == 1 ){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+            $password = hash('sha256', $password);
+
+            if ($data['password'] === $password){
+
+                $_SESSION['user'] = $data['pseudo'];
+                header('Location:landing.php');
+            }else header('Location:index.php?login_err=password');
+        }else header('Location:index.php?login_err=email');
+
+    }else header('Location:index.php?login_err=already');
+
+}else header('Location:index.php')
+?>
